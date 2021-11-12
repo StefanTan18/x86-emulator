63709d5d88103d5eb678bb8555239d9d51a79b01
Stefan Tan <stefantan39@yahoo.com>
2021-11-11 21:28:11 -0500

    can successfully read .com files

--- x86.c
+++ x86.c
@@ -7,7 +7,7 @@ byte mem[0x100000];
 
 byte AL, AH, BH, BL, CH, CL, DH, DL;
 word AX, BX, CX, DX;
-word IP, FLAGS;
+word IP, FLAGS, SP;
 word CS, DS, SS, ES;
 
 int main(int argc, char * argv[]) {
@@ -15,9 +15,9 @@ int main(int argc, char * argv[]) {
     DS = 0x1000;
     SS = 0x1000;
 
-    byte *code = mem[CS*16];
-    byte *data = mem[DS*16];
-    byte *stack = mem[SS*16];
+    byte *code = &mem[CS*16];
+    byte *data = &mem[DS*16];
+    byte *stack = &mem[SS*16];
 
     FILE *fp;
     char *filename;
@@ -39,17 +39,43 @@ int main(int argc, char * argv[]) {
         return 0;
     }
 
-    // Read File
-    char * line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    while ((read = getline(&line, &len, fp)) != -1) {
-        printf("Retrieved line of length %zu:\n", read);
-        printf("%s", line);
+    // Reads File into mem[CS*16+0x100]
+    fread(code + 0x100, sizeof(mem), 1, fp);
+
+    /*
+    // Test to make sure the .com file is read properly
+    for (int i = 0; i < 65; i++) {
+        if (i % 16 == 0) {
+            printf("\n");
+        }
+        if (mem[i] < 10) {
+            printf("0%x ", mem[i]);
+        }
+        else {
+            printf("%x ", mem[i]);
+        }
     }
+    printf("\n");
+    */
 
     fclose(fp);
 
+    // TESTING STUFF
+    // CURRENTLY JUST PRINTS OUT THE FIRST 16HEX, COMPARE AGAINST the Terminal Command: % hexdump (name of .com file)
+    byte codebyte;
+    IP = 0x100;
+    SP = 0xFFFE;
+
+    int i = 0;
+    while(i < 16) {
+        codebyte = mem[CS * 0x10 + IP];
+        IP++;
+        printf("%x ", codebyte);
+        i++;
+    }
+    printf("\n");
+
+/*
     byte codebyte;
     IP = 0x100;
     SP = 0xFFFE;
@@ -70,6 +96,7 @@ int main(int argc, char * argv[]) {
             break;
         }
     }
+*/
 
     return 0;
 }
